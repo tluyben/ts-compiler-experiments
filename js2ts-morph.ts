da1fe3b854e12deb96bc0c6a802f2aab800351c1
Tycho Luyben <tycho@e-lab.nl>
2023-06-09 08:10:45 +0200

    ts-morph was too hard to work with; went with vanilla compiler api

---
+++ js2ts-morph.ts
@@ -0,0 +1,85 @@
+import { Project, SyntaxKind, TransformTraversalControl, ts } from "ts-morph";
+import fs from "fs";
+
+const project = new Project({
+
+    tsConfigFilePath: "./tsconfig.json",
+    skipFileDependencyResolution: true,
+
+})
+
+const file = './parser.js'
+const sourceFile = project.addSourceFileAtPath(file)
+
+sourceFile.forEachDescendant((node, traversal) => {
+    if (node.getText().indexOf('initString') >= 0)
+        console.log(node.getKindName(), node.getKind(), node.getText().substring(0, 100))
+})
+
+if (false) try {
+    sourceFile.transform((traversal: TransformTraversalControl) => {
+        const node = traversal.visitChildren();
+
+        if (ts.isFunctionDeclaration(node)) {
+            const _parameters = node.parameters.map((p) => {
+                return traversal.factory.createParameterDeclaration(
+                    [],
+                    p.dotDotDotToken,
+                    p.name,
+                    p.questionToken,
+                    traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                    p.initializer
+                )
+            })
+
+            return traversal.factory.updateFunctionDeclaration(
+                node,
+                [],
+                node.asteriskToken,
+                node.name,
+                [],
+                _parameters,
+                traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                node.body
+            );
+        }
+
+        if (ts.isVariableDeclaration(node)) {
+            // skip variables with initializer as there the type is already known (is it?)
+            //if (!node.initializer) {
+            return traversal.factory.createVariableDeclaration(
+                node.name,
+                node.exclamationToken,
+                traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                node.initializer
+            )
+            //}
+        }
+
+        if (ts.isArrowFunction(node)) {
+            return traversal.factory.createArrowFunction(
+                node.modifiers,
+                node.typeParameters,
+                node.parameters.map((p) => {
+                    return traversal.factory.createParameterDeclaration(
+                        [],
+                        p.dotDotDotToken,
+                        p.name,
+                        p.questionToken,
+                        traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                        p.initializer
+                    )
+                }),
+                traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                node.equalsGreaterThanToken,
+                node.body
+            )
+        }
+        return node
+    })
+} catch (e: any) {
+    //console.log(e.message)
+}
+
+//console.log(sourceFile.getText())
+//fs.writeFileSync(file.replace('.js', '.ts'), sourceFile.getText())
--- js2ts.ts
+++ js2ts.ts
@@ -1,89 +1,132 @@
-import { Project, SyntaxKind, TransformTraversalControl, ts } from "ts-morph";
-import fs from "fs";
+import ts from 'typescript';
+import fs from 'fs';
 
-const project = new Project({
-
-    tsConfigFilePath: "./tsconfig.json",
-    skipFileDependencyResolution: true,
-
-})
-
-const file = './flop.js'
-const sourceFile = project.addSourceFileAtPath(file)
-
-sourceFile.forEachDescendant((node, traversal) => {
-    console.log(node.getKindName(), node.getText())
-})
-
-sourceFile.transform((traversal: TransformTraversalControl) => {
-    const node = traversal.visitChildren();
-
-    if (ts.isFunctionDeclaration(node)) {
-        const _parameters = node.parameters.map((p) => {
-            return traversal.factory.createParameterDeclaration(
-                [],
-                p.dotDotDotToken,
-                p.name,
-                p.questionToken,
-                traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
-                p.initializer
-            )
-        })
-
-        return traversal.factory.updateFunctionDeclaration(
-            node,
-            [],
-            node.asteriskToken,
-            node.name,
-            [],
-            _parameters,
-            traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
-            node.body
-        );
-    }
+function transformer(context: ts.TransformationContext) {
+    return (rootNode: ts.Node) => {
+        function visit(node: ts.Node): ts.Node {
 
 
-    return node
-})
+            if (ts.isVariableDeclaration(node)) {
 
-sourceFile.transform((traversal: TransformTraversalControl) => {
-    const node = traversal.visitChildren();
-
-    if (ts.isVariableDeclaration(node)) {
-        return traversal.factory.createVariableDeclaration(
-            node.name,
-            node.exclamationToken,
-            traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
-            node.initializer
-        )
-    }
-    return node
-})
-
-sourceFile.transform((traversal: TransformTraversalControl) => {
-    const node = traversal.visitChildren();
-
-    if (ts.isArrowFunction(node)) {
-        return traversal.factory.createArrowFunction(
-            node.modifiers,
-            node.typeParameters,
-            node.parameters.map((p) => {
-                return traversal.factory.createParameterDeclaration(
-                    [],
-                    p.dotDotDotToken,
-                    p.name,
-                    p.questionToken,
-                    traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
-                    p.initializer
+                node = ts.factory.createVariableDeclaration(
+                    node.name,
+                    node.exclamationToken,
+                    ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                    node.initializer
                 )
-            }),
-            traversal.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
-            node.equalsGreaterThanToken,
-            node.body
-        )
-    }
-    return node
-})
 
-console.log(sourceFile.getText())
-fs.writeFileSync(file.replace('.js', '.ts'), sourceFile.getText())
+            } else if (ts.isVariableStatement(node)) {
+
+                node = ts.factory.updateVariableStatement(
+                    node,
+                    node.modifiers,
+                    ts.factory.createVariableDeclarationList(
+                        node.declarationList.declarations.map((d) => {
+                            return ts.factory.createVariableDeclaration(
+                                d.name,
+                                d.exclamationToken,
+                                ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                                d.initializer
+                            )
+                        }),
+                        node.declarationList.flags
+                    )
+                )
+
+            } else if (ts.isMethodDeclaration(node)) {
+
+                node = ts.factory.updateMethodDeclaration(
+                    node,
+                    node.modifiers,
+                    node.asteriskToken,
+                    node.name,
+                    node.questionToken,
+                    node.typeParameters,
+                    node.parameters.map((p) => {
+                        return ts.factory.createParameterDeclaration(
+                            [],
+                            p.dotDotDotToken,
+                            p.name,
+                            p.questionToken,
+                            ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                            p.initializer
+                        )
+                    }),
+                    ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                    node.body
+                )
+
+
+            } else if (ts.isFunctionDeclaration(node)) {
+
+                const _parameters = node.parameters.map((p) => {
+                    return ts.factory.createParameterDeclaration(
+                        [],
+                        p.dotDotDotToken,
+                        p.name,
+                        p.questionToken,
+                        ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                        p.initializer
+                    )
+                })
+
+                node = ts.factory.updateFunctionDeclaration(
+                    node,
+                    [],
+                    node.asteriskToken,
+                    node.name,
+                    [],
+                    _parameters,
+                    ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                    node.body
+                );
+
+            } else if (ts.isArrowFunction(node)) {
+
+                node = ts.factory.createArrowFunction(
+                    node.modifiers,
+                    node.typeParameters,
+                    node.parameters.map((p) => {
+                        return ts.factory.createParameterDeclaration(
+                            [],
+                            p.dotDotDotToken,
+                            p.name,
+                            p.questionToken,
+                            ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                            p.initializer
+                        )
+                    }),
+                    ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                    node.equalsGreaterThanToken,
+                    node.body
+                )
+
+            } else {
+                //console.log(node.kind, node.getText()?.substring(0, 100))
+            }
+            return ts.visitEachChild(node, visit, context);
+        }
+
+        return ts.visitNode(rootNode, visit);
+    };
+}
+
+const file = './parser.js'
+const sourceFile = ts.createSourceFile(
+    file,
+    fs.readFileSync(file).toString(),
+    ts.ScriptTarget.ES2015,
+    /*setParentNodes */ true
+);
+
+const result = ts.transform(sourceFile, [transformer]);
+
+const printer = ts.createPrinter();
+
+result.transformed.forEach((n) => {
+    const output = printer.printFile(n as ts.SourceFile);
+    //console.log(output)
+    ts.sys.writeFile(file.replace('.js', '.ts'), output);
+});
+
+result.dispose();
